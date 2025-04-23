1901c796bc18e69f97bdb24e3e8b891dba72a826
lidawang <2632711107@qq.com>
2025-04-23 17:03:17 +0800

    Revert "mas测试"
    This reverts commit 68b05f82da8915b5513072cdaa69bd3434c8aa4f.

--- src/study/four.java
+++ src/study/four.java
@@ -4,7 +4,8 @@ package study;
 
 public class four {
     public static void main(String[] args) {
-        System.out.println("lidawnag");
+        System.out.println("ldw");
+        System.out.println("666");
     }
 
 }
