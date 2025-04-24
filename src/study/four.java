e4a6f4156502bae7ea7ed77b8978a95ff137fd04
lidawang <2632711107@qq.com>
2025-04-24 09:07:22 +0800

    mas424

--- src/study/four.java
+++ src/study/four.java
@@ -6,5 +6,8 @@ public class four {
     public static void main(String[] args) {
         System.out.println("ldw");
     }
-
+    public static boolean test(){
+        System.out.println("ldw");
+        return false;
+    }
 }
