dfe1c43dc7b22e512cef04551728f5623b800e7b
lidawang <2632711107@qq.com>
2025-04-24 09:17:52 +0800

    del4243

--- src/study/four.java
+++ src/study/four.java
@@ -6,7 +6,9 @@ public class four {
     public static void main(String[] args) {
         System.out.println("ldw");
         int ldw = ldw(3, 4);
-        System.out.println(ldw);
+        if("1".equals(String.valueOf(ldw))){
+            System.out.println(ldw);
+        }
     }
     public static int ldw(double a,double b){
         return (a>b)? 1:0;
