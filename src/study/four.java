6da90564c97ff8c99adef8a0935f70c0a29b73d3
lidawang <2632711107@qq.com>
2025-04-24 09:16:14 +0800

    del4242

--- src/study/four.java
+++ src/study/four.java
@@ -5,6 +5,8 @@ package study;
 public class four {
     public static void main(String[] args) {
         System.out.println("ldw");
+        int ldw = ldw(3, 4);
+        System.out.println(ldw);
     }
     public static int ldw(double a,double b){
         return (a>b)? 1:0;
