ee03d7a20466275b03728e00c0c6047b21827ee4
lidawang <2632711107@qq.com>
2025-04-24 09:24:23 +0800

    del4246

--- src/study/four.java
+++ src/study/four.java
@@ -5,11 +5,15 @@ package study;
 public class four {
     public static void main(String[] args) {
         System.out.println("ldw");
-        boolean test = test(3, 4);
-        if(test){
-            System.out.println(test);
+        int ldw = ldw(3, 4);
+        if("1".equals(String.valueOf(ldw))){
+            System.out.println(ldw);
         }
     }
+    public static int ldw(double a,double b){
+        return a>b? 1:0;
+    }
+
 
     public static boolean test(int a,int b){
         return b<a? true:false;
