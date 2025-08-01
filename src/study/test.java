85b0668e7cd66f9a54a9b951698c4247ba43ebdb
lidawang <2632711107@qq.com>
2025-08-01 17:36:53 +0800

    mas4247

---
+++ src/study/test.java
@@ -0,0 +1,15 @@
+package study;
+
+public class test {
+    public static void main(String[] args) {
+        String listType ="201";
+        String medChrgitmType ="01";
+
+        if(listType != "201" || !medChrgitmType.matches("03|02|12|04")){
+            System.out.println("自费");
+        }else{
+            System.out.println("报销");
+        }
+
+    }
+}
